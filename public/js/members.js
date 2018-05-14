4d3b3353b578009840b141ca28084a9876e73c3c
Alex Rosenkranz <alex.rosenkranz@gmail.com>
2018-05-14 17:35:48 -0400

    added bootstrap 4

--- public/js/members.js
+++ public/js/members.js
@@ -2,6 +2,9 @@ $(document).ready(function() {
   // This file just does a GET request to figure out which user is logged in
   // and updates the HTML on the page
   $.get("/api/user_data").then(function(data) {
+    console.log(data);
     $(".member-name").text(data.email);
+    $("#member-photo").attr("src", data.photo);
+
   });
 });
