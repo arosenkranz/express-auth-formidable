56c4e08d58e4689ba095cadaa3dbe840a369a66e
Alex Rosenkranz <alex.rosenkranz@gmail.com>
2018-12-01 09:03:48 -0500

    updated paths

--- models/user.js
+++ models/user.js
@@ -36,5 +36,6 @@ module.exports = function(sequelize, DataTypes) {
   User.hook("beforeCreate", function(user) {
     user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null);
   });
+  
   return User;
 };
